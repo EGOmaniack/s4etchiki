6228f25ae0d4eae6d6403d0a3b17c2523e6713a0
EGOmaniack <EGOmaniack@mail.ru>
2017-02-24 14:18:56 +1000

    It works
    \0/

--- web/ajax/workflow.php
+++ web/ajax/workflow.php
@@ -4,15 +4,13 @@ ini_set('xdebug.var_display_max_depth', 5);
 ini_set('xdebug.var_display_max_children', 256);
 ini_set('xdebug.var_display_max_data', 1024);
 
+$path = $_POST['path'];
+$curform = $_POST['state']['form'];
 
 $xmlka = file_get_contents("../workflow.xml");
-
 $xml = simplexml_load_string($xmlka);
-
 $defstate = (string)$xml->formlist->attributes()['start'];
-
 $forms;
-
 foreach($xml->formlist->form as $value) {
     foreach($value->state as $key => $elem){
         $form[(string)$elem->attributes()['btn']] = (string)$elem->attributes()['form'];
@@ -20,57 +18,19 @@ foreach($xml->formlist->form as $value) {
     $forms[((string)$value->attributes()['name'])]=$form;
     unset($form);
 }
-// var_dump($forms);
-// exit();
-
-
-// echo $_POST['state']['form'];
-// exit;
-// if(strlen($_POST['state']) > 0) {
-//     $state = $_POST['state'];
-// }
-
-$path = $_POST['path'];
-$curform = $_POST['state']['form'];
-// $path = 'next';
-// $curform = 's4etchiki';
-
 
 if($path == "start"){
     $state['form'] = $defstate;
+    $state['next'] = $forms[$defstate]['next'];
+    $state['back'] = null;
 }else{
     $state['form'] = $forms[$curform][$path];
-
-    // foreach($forms as $form){
-    //     if($form['name'] == $curform){
-    //         var_dump($form);
-    //     }
-    // }
+    $state['next'] = $forms[$forms[$curform][$path]]['next'];
+    $state['back'] = $forms[$forms[$curform][$path]]['back'];
 }
 
-// if($path == "start"){
-//     $state['form'] = "s4etchiki";
-//     $state['next'] = "graphic";
-//     $state['back'] = "";  
-// }
-// if($_POST['state']['form'] == 's4etchiki' && $path == 'next'){
-//     //$state['history'][0] = "s4etchiki";
-//     $state['form'] = "graphic";
-//     $state['next'] = "change";
-//     $state['back'] = "s4etchiki";
-// }
-// if($_POST['state']['form'] == 'graphic' && $path == 'back'){
-//     //$state['history'][0] = "graphic";
-//     $state['form'] = "s4etchiki";
-//     $state['next'] = "graphic";
-//     $state['back'] = "";
-// }
-
 $jstate = json_encode($state);
-// echo $state['form'];
-// echo <br>;
-// echo $path;
-//exit;
+
 header('Content-Type: application/json');
 echo $jstate;
 ?>
--- web/index.php
+++ web/index.php
@@ -1,28 +1,4 @@
 <?php
-
-// $dbconn = pg_connect("host=localhost port=5432 dbname=s4etchiki user=postgres password=Rgrur4frg56eq16")
-//     or die('Could not connect: ' . pg_last_error());
-
-
-// $result = pg_query($dbconn, "select id, to_char(\"date\",'DD-MM-YYYY') date , light_day, light_night, kichen_water_c, kichen_water_h, bath_water_c, bath_water_h from  s4etchiki.data ORDER BY id DESC LIMIT 1") or die('Ошибка запроса: ' . pg_last_error()); 
-
-// $id;
-// while ($line = pg_fetch_assoc($result)) {   
-//     foreach ($line as $col_key => $col_value ) { 
-//         if($col_key != "id")echo "\t<input id=".$id.$col_key." type='text' value=".$col_value."><lable>$col_key</lable>\n<br>";
-//         else
-//         {
-//             $id = $col_value;
-//              echo "\t <input type='hidden' id='dbid' value=".$col_value.">\n";
-//         }
-//     }
-//     echo "<hr>";
-// }
-// echo "<input type='button' value='Изменить' id='btnupdate'>\n";
-
-// pg_free_result($result);
-// pg_close($dbconn);
-
 //генерируем число
 $sek = strtotime("now");
 ?>
@@ -35,23 +11,22 @@ $sek = strtotime("now");
         <title>Счетчики</title>
     </head>
     <body>
-
         <h2>Контроль показателей</h2>
         <input class="btn back" type="button" value="Назад">
         <input class="btn next" type="button" value="Вперед">
         
         <h2 id='title'></h2>
-        <div id='content'>
-        </div>
+        <div id='content'></div>
         
         <script type="text/javascript" src = "js/main.js?<?=$sek?>" ></script>
+
         <script>
         window.state = "";
 
         
-        
             function s4etchiki (){
                 $("#content").html('');
+                //var form = '<h2>Новые данные</h2>';
                 var form ='<input placeholder="Свет день" type="number" id="light_day">';
                 form += '<br><input placeholder="Свет ночь" type="number" id="light_night">';
                 form += '<br><input placeholder="вода кухня холодная" type="number" id="kichen_water_c">';
@@ -79,14 +54,16 @@ $sek = strtotime("now");
             
             function changedata (){
                 $("#content").html('');
-                $.getJSON("/ajax/lastdata.php", function (data) {
-                    console.log("data");
+                $.get("/ajax/lastdata.php", function (data) {
                     $("#content").html(data);
                 });
                 $("#title").val('Предыдущие данные');
             }
             
             function changestate (data){
+                //кнопки back и next отображать не?
+                if(data.next == null){$('.next').addClass('hide');}else{$('.next').removeClass('hide');}
+                if(data.back == null){$('.back').addClass('hide');}else{$('.back').removeClass('hide');}
                 if(data.form == 's4etchiki'){
                     s4etchiki();
                 }
