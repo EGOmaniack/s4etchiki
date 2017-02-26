7d8720ac579f06a5a2677db9c5ad9824a32bd534
EGOmaniack <EGOmaniack@mail.ru>
2017-02-26 15:13:21 +1000

    ковыряем workflow

--- web/modules/s4etchiki/workflow.php
+++ web/modules/s4etchiki/workflow.php
@@ -8,21 +8,18 @@ $path = $_POST['path'];
 $curform = $_POST['state']['form'];
 
 $xmlka = file_get_contents("workflow.xml");
-
-// $a = json_decode(json_encode((array) simplexml_load_string($xmlka)),1);
-// var_dump($a);
-// exit;
 $xml = simplexml_load_string($xmlka);
-$defstate = (string)$xml->formlist->attributes()['start'];
+
+var_dump($xml->workflow->s4etchiki);
 $forms;
-foreach($xml->formlist->form as $value) {
-    foreach($value->state as $elem){
-        $form[(string)$elem->attributes()['btn']]['state'] = (string)$elem->attributes()['form'];
-    }
-    $forms[((string)$value->attributes()['name'])]=$form;
-    unset($form);
-}
-$_SESSION['workflow'] = $forms;
+// foreach($xml->formlist->form as $value) {
+//     foreach($value->state as $elem){
+//         $form[(string)$elem->attributes()['btn']]['state'] = (string)$elem->attributes()['form'];
+//     }
+//     $forms[((string)$value->attributes()['name'])]=$form;
+//     unset($form);
+// }
+// $_SESSION['workflow'] = $forms;
 // $include = 'orcestration/'.$_SESSION['workflow'][$_GET['flow']]['orcestration']['init'].'.php';
 // echo $include;
 // exit;
