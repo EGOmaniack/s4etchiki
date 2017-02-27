637981096d4d502fe91ccccaa17ca7e12360c77f
AVVlasov <lizmatik@yandex.ru>
2017-02-27 23:12:33 +0300

    немного магии)))

--- web/modules/graphic/workflow.php
+++ web/modules/graphic/workflow.php
@@ -5,6 +5,14 @@ ini_set('xdebug.var_display_max_children', 256);
 ini_set('xdebug.var_display_max_data', 1024);
 
 $xmlka = file_get_contents(dirname(__FILE__)."/workflow.xml");
+
+$json = XML2JSON($xmlka);
+$arr = json_decode($json, true);
+var_dump($arr);
+exit;
+
+
+/*
 $xml = simplexml_load_string($xmlka);
 
 $screens;
@@ -13,8 +21,9 @@ foreach($xml->graphic as $value) {
         if((string)$value->attributes()['start'] == 'true'){
             $screens['start'] = ((string)$value->attributes()['name']);
         }
-    foreach($value->state as $elem){
         $screen['name'] = (string)$value->attributes()['name'];
+        var_dump($value->state);
+    foreach($value->state as $key => $elem){       
         $screen['state']['go_next'] = (string)$elem->attributes()['go-next'];
         $screen['state']['go_back'] = (string)$elem->attributes()['go-back'];
     }
@@ -25,9 +34,36 @@ foreach($xml->graphic as $value) {
     $screens[((string)$value->attributes()['name'])]=$screen;
     unset($screen);
 }
-var_dump($screens);
+*/
+//var_dump($screens);
 $_SESSION['workflow'] = $screens;
 // $include = 'orcestration/'.$_SESSION['workflow'][$_GET['flow']]['orcestration']['init'].'.php';
 // echo $include;
 // exit;
+
+
+function XML2JSON($xml) {
+
+        function normalizeSimpleXML($obj, &$result) {
+            $data = $obj;
+            if (is_object($data)) {
+                $data = get_object_vars($data);
+            }
+            if (is_array($data)) {
+                foreach ($data as $key => $value) {
+                    $res = null;
+                    normalizeSimpleXML($value, $res);
+                    if (($key == '@attributes') && ($key)) {
+                        $result = $res;
+                    } else {
+                        $result[$key] = $res;
+                    }
+                }
+            } else {
+                $result = $data;
+            }
+        }
+        normalizeSimpleXML(simplexml_load_string($xml), $result);
+        return json_encode($result);
+    }
 ?>
